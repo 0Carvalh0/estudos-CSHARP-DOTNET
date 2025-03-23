aafbef3ac7964c477e66a411c8723f0a8e7d6461
devictor <trabalho.joaovictor2004@gmail.com>
2025-03-23 14:23:07 -0300

    feat: Aula 03

---
+++ Aula03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula03;
+
+public class Program {
+  public static void Main(string [] args) {
+    // const
+    // var
+    // dynamic
+
+      const int number = 999;
+
+      number = 2000;
+
+      Console.WriteLine("O valor da constante é: " + number);
+
+      var name = "None";
+
+      Console.WriteLine("O valor da variavel é: " + name);
+
+      dynamic test = 20;
+
+      dynamic = "Antonio";
+  }
+}
